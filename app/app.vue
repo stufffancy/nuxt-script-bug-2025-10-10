76a13aa8c81d657a721aa83b6c59146bf20ddc04
lfx <dostufffancy@qq.com>
2025-10-10 11:49:40 +0800

    init

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const script = useScript({
+  src: 'https://example.com/analytics.js',
+}, {
+  // https://scripts.nuxt.com/docs/api/use-script-trigger-idle-timeout
+  trigger: useScriptTriggerIdleTimeout({ timeout: 5000 })
+})
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
